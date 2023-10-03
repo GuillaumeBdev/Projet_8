861cee439af16573f5effa04ec02bbbc1de45b50
GuillaumeBdev <gblanc.dev@gmail.com>
2023-10-03 16:12:45 +0200

    Some sass adjustment

--- src/components/main/card.js
+++ src/components/main/card.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const Cards = ({id, title, cover}) => {
+const Cards = ({id, title, cover, location}) => {
+    console.log(location)
     return (
         <NavLink to={`/fiche/${id}`} className='lien-location'>
             <figure className='figure-location'>
                 <img className='img-location' src={cover} alt={title}/>
                 <figcaption className='titre-location'>{title}</figcaption>
+<div className='bg'></div>
 
             </figure> 
         </NavLink>
